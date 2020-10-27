30e70cd5cf4513386a809fb9f6f9fd7220dc52c0
Juliette <julietteredheadgirl@yahoo.com>
2020-10-27 11:32:23 -0700

    first commit

---
+++ 0-start.md
@@ -0,0 +1,29 @@
+# Angular 10 Tutorial on Localization with Transloco
+
+https://medium.com/i18n-and-l10n-resources-for-developers/angular-10-tutorial-on-localization-with-transloco-3b7cc5c2ff65
+
+
+- `Transloco` => an internationalization (i18n) library for Angular => define the content of your app in different languages 
+
+- `docs` => https://ngneat.github.io/transloco/
+
+```yaml
+ng add @ngneat/transloco
+
+ng add ngx-bootstrap
+
+ng g c nav-bar --module app
+
+ng g c feedback --module app
+
+# use Phrase to translate the content from the en.json file into desired language
+
+# Login to Phrase => create new project
+
+# execute the app in your local environment:
+ng serve -o
+
+# Production Build
+ng build --prod
+
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco/transloco-root.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FormsModule } from '@angular/forms';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FeedbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslocoRootModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
